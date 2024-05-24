a322ac2d47cb64a5c318d7a504f73f5fc0ffec0b
Justin Simon <jtsimon@jtsimon.dev>
2024-05-23 20:00:03 -0400

    Add address services to DI

--- include/singleton_injector.h
+++ include/singleton_injector.h
@@ -9,17 +9,24 @@
 #include "icontact_service.h"
 #include "contact_repository.h"
 #include "contact_service.h"
+#include "iaddress_repository.h"
+#include "iaddress_service.h"
+#include "address_repository.h"
+#include "address_service.h"
 #include "utility.h"
 
 namespace di = boost::di;
 
 class SingletonInjector {
 public:
-    static auto& getInjector() {
+    static auto &getInjector() {
         static auto injector = di::make_injector(
                 di::bind<std::string>.to(getDbPath()),
                 di::bind<IContactRepository>().to<ContactRepository>(),
-                di::bind<IContactService>().to<ContactService>()
+                di::bind<IContactService>().to<ContactService>(),
+                di::bind<IAddressRepository>().to<AddressRepository>(),
+                di::bind<IAddressService>().to<AddressService>()
+
         );
         return injector;
     }
@@ -35,9 +42,12 @@ private:
     }
 
     SingletonInjector() = default;
+
     ~SingletonInjector() = default;
-    SingletonInjector(const SingletonInjector&) = delete;
-    SingletonInjector& operator=(const SingletonInjector&) = delete;
+
+    SingletonInjector(const SingletonInjector &) = delete;
+
+    SingletonInjector &operator=(const SingletonInjector &) = delete;
 };
 
 #endif // PRM_SINGLETON_INJECTOR_H
